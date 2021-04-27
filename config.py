0b9837d8bdd377377aaee5da4bed695ca3aec05b
VentCoul <73941416+VentCoul@users.noreply.github.com>
2021-04-27 15:26:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = 'bot token'
-TIMEZONE = 'Europe/Kiev'
+TOKEN = 'bot token'  # bot token - ваш телеграм токен который вы получили после регистрации бота в @BotFather
+TIMEZONE = 'Europe/Kiev' 
 TIMEZONE_COMMON_NAME = 'Kiev'
 
